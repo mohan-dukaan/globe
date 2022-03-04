04272fb4598716c67bf94b0e162fb5d305c4e1cc
Mohan Dukaan <mohan.kumar@rankz.io>
2022-03-04 14:14:34 +0530

    customization changes updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
+import { COUNTRIES_DATA } from "./data/countries_data.js";
+import HEX_DATA from "./data/countries_hex_data.json";
+import Globe from "react-globe.gl";
 
-function App() {
+export default function App() {
+  const globeEl = useRef();
+  const draggingRef = useRef(0);
+
+  const [hex, setHex] = useState({ features: [] });
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setHex(HEX_DATA);
+  }, []);
+
+  useEffect(() => {
+    globeEl.current.controls().autoRotateSpeed = 0.5;
+    var canvas = document.getElementsByTagName("canvas")[0];
+    canvas.addEventListener("mousedown", () => {
+      draggingRef.current++;
+    });
+    canvas.addEventListener("mouseup", () => {
+      setTimeout(() => {
+        draggingRef.current--;
+        if (!draggingRef.current) {
+          setFoucus(index);
+        }
+      }, 5000);
+    });
+  }, [globeEl]);
+
+  useEffect(() => {
+    if (!draggingRef.current) {
+      setFoucus(index);
+    }
+  }, [globeEl, index]);
+
+  const setFoucus = (index = 0) => {
+    console.log("dara", draggingRef.current);
+    if (!draggingRef.current) {
+      const MAP_CENTER = { ...COUNTRIES_DATA[index], altitude: 0.4 };
+      globeEl.current.pointOfView(MAP_CENTER, 2000);
+      setTimeout(() => {
+        if (!draggingRef.current) {
+          globeEl.current.pointOfView({ ...MAP_CENTER, altitude: 1.5 }, 2000);
+          setTimeout(() => {
+            if (!draggingRef.current) {
+              var idx = index + 1 >= COUNTRIES_DATA.length ? 0 : index + 1;
+              setIndex(idx);
+            }
+          }, 3000);
+        }
+      }, 5000);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Globe
+      ref={globeEl}
+      globeImageUrl="/transparent.png"
+      backgroundColor="#fff"
+      labelsData={COUNTRIES_DATA}
+      labelAltitude={0.05}
+      hexPolygonsData={hex.features}
+      hexPolygonResolution={4}
+      hexPolygonMargin={0.62}
+      hexPolygonColor={useCallback(() => "#006ee6", [])}
+      labelLat={(d) => d.lat}
+      labelLng={(d) => d.lng}
+      labelText={(d) => d.name}
+      labelSize={(d) => 0.8}
+      labelDotRadius={(d) => 0.5}
+      labelColor={() => "#FF0000"}
+      hexTopColor={() => "#FF0000"}
+      hexSideColor={() => "#006ee6"}
+      labelResolution={2}
+      hexBinPointsData={COUNTRIES_DATA}
+      hexBinPointWeight="size"
+      hexAltitude={(d) => 0.04}
+      hexBinResolution={4}
+      hexBinMerge={true}
+      enablePointerInteraction={false}
+    />
   );
 }
-
-export default App;
---
+++ src/data/countries_data.js
@@ -0,0 +1,441 @@
+export const COUNTRIES_DATA = [
+    {
+      country: "AE",
+      lat: 23.424076,
+      lng: 53.847818,
+      name: "United Arab Emirates"
+    },
+    {
+      country: "AO",
+      lat: -11.202692,
+      lng: 17.873887,
+      name: "Angola"
+    },
+    {
+      country: "AR",
+      lat: -38.416097,
+      lng: -63.616672,
+      name: "Argentina"
+    },
+    {
+      country: "AT",
+      lat: 47.516231,
+      lng: 14.550072,
+      name: "Austria"
+    },
+    {
+      country: "AU",
+      lat: -25.274398,
+      lng: 133.775136,
+      name: "Australia"
+    },
+    {
+      country: "BD",
+      lat: 23.684994,
+      lng: 90.356331,
+      name: "Bangladesh"
+    },
+    {
+      country: "BE",
+      lat: 50.503887,
+      lng: 4.469936,
+      name: "Belgium"
+    },
+    {
+      country: "BG",
+      lat: 42.733883,
+      lng: 25.48583,
+      name: "Bulgaria"
+    },
+    {
+      country: "BR",
+      lat: -14.235004,
+      lng: -51.92528,
+      name: "Brazil"
+    },
+    {
+      country: "CA",
+      lat: 56.130366,
+      lng: -106.346771,
+      name: "Canada"
+    },
+    {
+      country: "CH",
+      lat: 46.818188,
+      lng: 8.227512,
+      name: "Switzerland"
+    },
+    {
+      country: "CL",
+      lat: -35.675147,
+      lng: -71.542969,
+      name: "Chile"
+    },
+    {
+      country: "CN",
+      lat: 35.86166,
+      lng: 104.195397,
+      name: "China"
+    },
+    {
+      country: "CO",
+      lat: 4.570868,
+      lng: -74.297333,
+      name: "Colombia"
+    },
+    {
+      country: "CR",
+      lat: 9.748917,
+      lng: -83.753428,
+      name: "Costa Rica"
+    },
+    {
+      country: "CZ",
+      lat: 49.817492,
+      lng: 15.472962,
+      name: "Czech Republic"
+    },
+    {
+      country: "DE",
+      lat: 51.165691,
+      lng: 10.451526,
+      name: "Germany"
+    },
+    {
+      country: "DK",
+      lat: 56.26392,
+      lng: 9.501785,
+      name: "Denmark"
+    },
+    {
+      country: "DO",
+      lat: 18.735693,
+      lng: -70.162651,
+      name: "Dominican Republic"
+    },
+    {
+      country: "DZ",
+      lat: 28.033886,
+      lng: 1.659626,
+      name: "Algeria"
+    },
+    {
+      country: "EC",
+      lat: -1.831239,
+      lng: -78.183406,
+      name: "Ecuador"
+    },
+    {
+      country: "EG",
+      lat: 26.820553,
+      lng: 30.802498,
+      name: "Egypt"
+    },
+    {
+      country: "ES",
+      lat: 40.463667,
+      lng: -3.74922,
+      name: "Spain"
+    },
+    {
+      country: "ET",
+      lat: 9.145,
+      lng: 40.489673,
+      name: "Ethiopia"
+    },
+    {
+      country: "FI",
+      lat: 61.92411,
+      lng: 25.748151,
+      name: "Finland"
+    },
+    {
+      country: "FR",
+      lat: 46.227638,
+      lng: 2.213749,
+      name: "France"
+    },
+    {
+      country: "GB",
+      lat: 55.378051,
+      lng: -3.435973,
+      name: "United Kingdom"
+    },
+    {
+      country: "GH",
+      lat: 7.946527,
+      lng: -1.023194,
+      name: "Ghana"
+    },
+    {
+      country: "GR",
+      lat: 39.074208,
+      lng: 21.824312,
+      name: "Greece"
+    },
+    {
+      country: "GT",
+      lat: 15.783471,
+      lng: -90.230759,
+      name: "Guatemala"
+    },
+    {
+      country: "HK",
+      lat: 22.396428,
+      lng: 114.109497,
+      name: "Hong Kong"
+    },
+    {
+      country: "HU",
+      lat: 47.162494,
+      lng: 19.503304,
+      name: "Hungary"
+    },
+    {
+      country: "ID",
+      lat: -0.789275,
+      lng: 113.921327,
+      name: "Indonesia"
+    },
+    {
+      country: "IE",
+      lat: 53.41291,
+      lng: -8.24389,
+      name: "Ireland"
+    },
+    {
+      country: "IL",
+      lat: 31.046051,
+      lng: 34.851612,
+      name: "Israel"
+    },
+    {
+      country: "IN",
+      lat: 20.593684,
+      lng: 78.96288,
+      name: "India"
+    },
+    {
+      country: "IT",
+      lat: 41.87194,
+      lng: 12.56738,
+      name: "Italy"
+    },
+    {
+      country: "JP",
+      lat: 36.204824,
+      lng: 138.252924,
+      name: "Japan"
+    },
+    {
+      country: "KE",
+      lat: -0.023559,
+      lng: 37.906193,
+      name: "Kenya"
+    },
+    {
+      country: "KR",
+      lat: 35.907757,
+      lng: 127.766922,
+      name: "South Korea"
+    },
+    {
+      country: "KW",
+      lat: 29.31166,
+      lng: 47.481766,
+      name: "Kuwait"
+    },
+    {
+      country: "LK",
+      lat: 7.873054,
+      lng: 80.771797,
+      name: "Sri Lanka"
+    },
+    {
+      country: "LT",
+      lat: 55.169438,
+      lng: 23.881275,
+      name: "Lithuania"
+    },
+    {
+      country: "LU",
+      lat: 49.815273,
+      lng: 6.129583,
+      name: "Luxembourg"
+    },
+    {
+      country: "MA",
+      lat: 31.791702,
+      lng: -7.09262,
+      name: "Morocco"
+    },
+    {
+      country: "MO",
+      lat: 22.198745,
+      lng: 113.543873,
+      name: "Macau"
+    },
+    {
+      country: "MX",
+      lat: 23.634501,
+      lng: -102.552784,
+      name: "Mexico"
+    },
+    {
+      country: "MY",
+      lat: 4.210484,
+      lng: 101.975766,
+      name: "Malaysia"
+    },
+    {
+      country: "NG",
+      lat: 9.081999,
+      lng: 8.675277,
+      name: "Nigeria"
+    },
+    {
+      country: "NL",
+      lat: 52.132633,
+      lng: 5.291266,
+      name: "Netherlands"
+    },
+    {
+      country: "NO",
+      lat: 60.472024,
+      lng: 8.468946,
+      name: "Norway"
+    },
+    {
+      country: "NZ",
+      lat: -40.900557,
+      lng: 174.885971,
+      name: "New Zealand"
+    },
+    {
+      country: "PA",
+      lat: 8.537981,
+      lng: -80.782127,
+      name: "Panama"
+    },
+    {
+      country: "PE",
+      lat: -9.189967,
+      lng: -75.015152,
+      name: "Peru"
+    },
+    {
+      country: "PH",
+      lat: 12.879721,
+      lng: 121.774017,
+      name: "Philippines"
+    },
+    {
+      country: "PL",
+      lat: 51.919438,
+      lng: 19.145136,
+      name: "Poland"
+    },
+    {
+      country: "PR",
+      lat: 18.220833,
+      lng: -66.590149,
+      name: "Puerto Rico"
+    },
+    {
+      country: "PT",
+      lat: 39.399872,
+      lng: -8.224454,
+      name: "Portugal"
+    },
+    {
+      country: "QA",
+      lat: 25.354826,
+      lng: 51.183884,
+      name: "Qatar"
+    },
+    {
+      country: "RO",
+      lat: 45.943161,
+      lng: 24.96676,
+      name: "Romania"
+    },
+    {
+      country: "RU",
+      lat: 61.52401,
+      lng: 105.318756,
+      name: "Russia"
+    },
+    {
+      country: "SA",
+      lat: 23.885942,
+      lng: 45.079162,
+      name: "Saudi Arabia"
+    },
+    {
+      country: "SE",
+      lat: 60.128161,
+      lng: 18.643501,
+      name: "Sweden"
+    },
+    {
+      country: "SG",
+      lat: 1.352083,
+      lng: 103.819836,
+      name: "Singapore"
+    },
+    {
+      country: "SI",
+      lat: 46.151241,
+      lng: 14.995463,
+      name: "Slovenia"
+    },
+    {
+      country: "SK",
+      lat: 48.669026,
+      lng: 19.699024,
+      name: "Slovakia"
+    },
+    {
+      country: "TH",
+      lat: 15.870032,
+      lng: 100.992541,
+      name: "Thailand"
+    },
+    {
+      country: "TR",
+      lat: 38.963745,
+      lng: 35.243322,
+      name: "Turkey"
+    },
+    {
+      country: "TW",
+      lat: 23.69781,
+      lng: 120.960515,
+      name: "Taiwan"
+    },
+    {
+      country: "TZ",
+      lat: -6.369028,
+      lng: 34.888822,
+      name: "Tanzania"
+    },
+    {
+      country: "UY",
+      lat: -32.522779,
+      lng: -55.765835,
+      name: "Uruguay"
+    },
+    {
+      country: "VN",
+      lat: 14.058324,
+      lng: 108.277199,
+      name: "Vietnam"
+    },
+    {
+      country: "ZA",
+      lat: -30.559482,
+      lng: 22.937506,
+      name: "South Africa"
+    }
+  ];
+  
